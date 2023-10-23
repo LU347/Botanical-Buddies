5cf093d376a38a15cc0ae7b11c1ee5f865298fbe
Lulu <ongelishalu@gmail.com>
2023-10-23 00:01:26 -0500

    Add to Cart button functionality
    outputs to pseudocart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,24 @@
-//const fs = require('fs');
+//const fs = require('fs');  //script breaks if i use this :/
 //const jsonData = fs.readFileSync('plants.json');
 //const plantData = JSON.parse(products);
 
+//temp until i fix ^^
 const products = [
-    { "name": "Rose", "description": "A beautiful and fragrant flowering plant", "price": "9.99", "image": "https://example.com/rose.jpg"},
-    { "name": "Fern", "description": "A leafy green plant that thrives in shade", "price": "6.99", "image": "https://example.com/fern.jpg"}
+    { "name": "Rose", "description": "A beautiful and fragrant flowering plant", "price": "9.99", "image": "images/default.png"},
+    { "name": "Fern", "description": "A leafy green plant that thrives in shade", "price": "6.99", "image": "images/default.png"},
+    { "name": "Lavender", "description": "A purple flowering plant known for its soothing scent", "price": "7.99", "image": "images/default.png"},
 ]
-
 const container = document.getElementById('container');
 const plantObjects = [];
+
+const productsInCart = [];      //for the fakeCart
 
 products.forEach(Plant => {
     const plantObject = {
         name: Plant.name,
         description: Plant.description,
-        price: Plant.price
+        price: Plant.price,
+        image: Plant.image
     };
     plantObjects.push(plantObject);
 });
@@ -29,21 +33,31 @@ function createCard(plant) {
     plantElement.innerHTML = `
     <div class="card">
         <div class="cardbody">
-            <img src="images/hibiscus.avif" alt="" class="cardimage" width="150px">
+            <img src="${plant.image}" alt="" class="cardimage" width="150px">
                 <h2 id="itemName" class="cardtitle">${plant.name}</h2>
                 <p id="itemDesc" class="carddescription">${plant.description}</p>
                 <p id="itemPrice" class="cardprice">${plant.price}</p>
         </div>
-        <button onclick="addToCart()" class="cardbutton">Add to cart</button>
+        <button class="cardbutton">Add to cart</button>
     </div>
     `;
+
+    const addToCartButton = plantElement.querySelector('.cardbutton');
+    addToCartButton.addEventListener('click', () => addToCart(plant));
 
     cardElement.appendChild(plantElement);
     return cardElement;
 }
 
 function addToCart(product) {
-    alert(product);
+    var fakeCart = document.getElementById('fakeCart'); //temp
+    var msgElement = document.createElement('div');
+    
+    msgElement.textContent = product.name + "," + product.description + "," + product.price;
+    fakeCart.appendChild(msgElement);
+
+    productsInCart.push(product);
+    console.log(productsInCart);
 }
 
 function displayCards() {

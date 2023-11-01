0b65c5cc76ce8cc48061a176280bfed6b95c0518
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-11-01 11:04:57 -0500

    Create connect.php

---
+++ assets/js/connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_host="partygoer.mysql.database.azure.com";        
+$db_user="matthewmartinez";        //Change this
+$db_pass="1qaz2wsx!QAZ@WSX";        //Change this
+$db_name="herewego";     //Do not change
+
+$db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if (!$db_conn) {
+    die("Error: " . mysqli_connect_error());
+}
+else
+{
+  echo 'Login Successful';
+}
+
+
+?>

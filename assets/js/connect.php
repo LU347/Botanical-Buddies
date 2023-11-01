1deb9b32fff655ee3a64816ad62ed336040fa0ed
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2023-11-01 11:46:40 -0500

    Update connect.php
    Re-update

--- assets/js/connect.php
+++ assets/js/connect.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $db_host="partygoer.mysql.database.azure.com";        
@@ -10,13 +11,22 @@ $db_conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if (!$db_conn) {
     die("Error: " . mysqli_connect_error());
 }
-else
-{
-    header("Location: index.html");
+
+$USERNAME = mysqli_real_escape_string($db_conn, $_GET['fname']); // Sanitize input
+$PASSWORD = mysqli_real_escape_string($db_conn, $_GET['lname']); // Sanitize input
+
+
+$query = "SELECT * FROM login WHERE username = '$USERNAME' AND password = '$PASSWORD'";
+$result = mysqli_query($db_conn, $query);
+if (mysqli_num_rows($result) > 0) {
+  echo 'Login Successful';
+
+   // header("Location: index.html");
     // possible JS script method if Header doesn't work
     //<script type="text/javascript"> 
     //window.location.href="Index.html" 
     //</script> 
+  
 }
 
 

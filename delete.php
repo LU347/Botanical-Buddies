2e620e2f086554f169d68476cdb4c116fe46595a
matttiepie <124634024+matttiepie@users.noreply.github.com>
2023-11-21 16:13:36 -0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@ $db_host="partygoer.mysql.database.azure.com";
 $db_user="matthewmartinez";        
 $db_pass="1qaz2wsx!QAZ@WSX";        
 $db_name="herewego"; 
-$USERNAME=$_POST['email'];
+$USERNAME=$_POST['name'];
     
 $connection = mysqli_connect("$db_host", "$db_user", "$db_pass", "$db_name");
 if (!$connection) {
